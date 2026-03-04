0205b360dbfb0baabbc67377fa91afbd76aa72fd
Escarleto <lucascliberato31@gmail.com>
2026-03-04 16:44:08 +0000

    Challenge2 completo

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool canShootdog = true;
+    private const float dogCooldown = 1f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && canShootdog)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            canShootdog = false;
+            Invoke("ResetDog", dogCooldown);
+        }
+    }
+
+    private void ResetDog()
+    {
+        canShootdog = true;
+    }
+}
